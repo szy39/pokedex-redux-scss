99e72cf1b2760a98a407a177563ce2e0ea87c0a3
szy39 <138227093+szy39@users.noreply.github.com>
2025-08-21 13:40:46 +0300

    updated main

---
+++ src/app/reducers/getPokemonData.ts
@@ -0,0 +1,9 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { genericPokemonType } from "../../utils/Types";
+
+export const getPokemonData = createAsyncThunk("pokemon/randomPokemon",async(pokemons:genericPokemonType[])=>{
+    try{
+        console.log({pokemons},"from reducer")
+    }catch(err){}
+
+})
