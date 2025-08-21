5bd2ebd62133e3af7cc575d66cf1a7f40f468199
szy39 <138227093+szy39@users.noreply.github.com>
2025-08-21 14:21:16 +0300

    fix: created api calls reducers and slices

--- src/app/slices/PokemonSlice.ts
+++ src/app/slices/PokemonSlice.ts
@@ -1,9 +1,12 @@
 import { PokemonTypeInitialState } from "../../utils/Types";
 import { createSlice } from "@reduxjs/toolkit";
 import { getInitialPokemonData } from "../reducers/getInitialPokemonData";
+import { getPokemonData } from "../reducers/getPokemonData";
+
 
 const initialState:PokemonTypeInitialState ={
-    allPokemon: undefined
+    allPokemon: undefined,
+    randomPokemons:undefined
 }
 
 
@@ -15,6 +18,9 @@ export const PokemonSlice = createSlice({
     extraReducers: (builder) => {
         builder.addCase(getInitialPokemonData.fulfilled,(state,action)=>{
             state.allPokemon = action.payload;
+        })
+        builder.addCase(getPokemonData.fulfilled,(state,action)=>{
+            state.randomPokemons = action.payload
         })
     }
 });
